4ef7f25493bd2a6eec63fab1f7db2f79c5b26c17
Matheus Igor Franchini Zaniboni <matheusifz@gmail.com>
2024-05-06 13:26:58 -0300

    createUpdateUser

--- app.py
+++ app.py
@@ -14,4 +14,11 @@ def create_user(name, age):
      cursor.execute('''''')
      connection.commit()
      connection.close()   
-        
+
+
+def update_user(id, name, age):
+    connection = sqlite3.connect('.db')
+    cursor = connection.cursor()
+    cursor.execute('''''')
+    connection.commit()   
+    connection.close()     
