e7e5a24bb110a4b12171f7d512515e9b448f5050
Matheus Igor Franchini Zaniboni <matheusifz@gmail.com>
2024-05-06 13:22:35 -0300

    createTableFuncition

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hi")
+import sqlite3
+
+def create_table():
+    connection = sqlite3.connect('.db')
+    cursor = connection.cursor()
+    cursor.execute('''''')
+    connection.commit()
+    connection.close()
+    
+
