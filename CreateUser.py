3a880cb05dd0039d3d2df82d095953f7715f7820
Matheus Igor Franchini Zaniboni <matheusifz@gmail.com>
2024-05-08 13:35:16 -0300

    addIdparameter

--- CreateUser.py
+++ CreateUser.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-def create_user(name, age):
-     connection = sqlite3.connect('.db')
+def create_user(id, name, age):
+     connection = sqlite3.connect('database.db')
      cursor = connection.cursor()
-     cursor.execute('''INSERT INTO users (name, age) VALUES (?, ?)''')
+     cursor.execute('''INSERT INTO user (id, name, age) VALUES (?, ?, ?)''', ('id 123', 'John', 26))
      connection.commit()
      connection.close()   
